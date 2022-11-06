d75218f8c5a04b09e616f4bf5c6976ae5403fd83
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-06 16:11:43 +0100

    Working on Hero

--- src/Hero.js
+++ src/Hero.js
@@ -7,7 +7,27 @@ export const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-center">
-        <article></article>
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            I'm baby twee lyft vape blue bottle keytar Brooklyn pork belly
+            vaporware man braid art party gluten-free. Before they sold out
+            kinfolk asymmetrical, sriracha XOXO beard subway tile salvia 8-bit
+            yuccie. Wolf truffaut man braid, sriracha YOLO salvia crucifix
+            pop-up pickled. Distillery locavore tattooed williamsburg mixtape
+            cornhole. Bespoke next level DIY kitsch sartorial master cleanse
+            migas. Kogi butcher literally sustainable cliche.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img
+            src={Phone}
+            className="phone-img"
+            alt="hero-img"
+            loading="lazy"
+          />
+        </article>
       </div>
     </section>
   );
