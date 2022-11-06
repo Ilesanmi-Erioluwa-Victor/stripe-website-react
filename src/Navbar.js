32a77d661ce71de37b33e9a5f07e463fb421b919
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-06 15:48:16 +0100

    Working on Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,7 +12,18 @@ export const Navbar = () => {
         <div className="nav-header">
           {/* <img src={logo} alt="stripe" loading="lazy" className="nav-logo" /> */}
           <h2>Stripe</h2>
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
         </div>
+
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">Products</button>
+            <button className="link-btn">Developers</button>
+            <button className="link-btn">Company</button>
+          </li>
+        </ul>
       </div>
     </nav>
   );
