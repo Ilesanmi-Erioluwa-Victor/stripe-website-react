7544e98f4da995667be0af2cd13bdbabf30511e5
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-06 15:38:53 +0100

    Working on Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import logo from "./images/logo.svg";
 
 export const Navbar = () => {
+  const { openSidebar, closeModal, openModal } = useGlobalContext();
   return <div>Navbar</div>;
 };
