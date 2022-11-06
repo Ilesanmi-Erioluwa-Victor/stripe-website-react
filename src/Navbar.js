668af52c03f6139fb3250a63484863a5f6ae6d5c
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-06 15:43:02 +0100

    Working on Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,5 +5,15 @@ import logo from "./images/logo.svg";
 
 export const Navbar = () => {
   const { openSidebar, closeModal, openModal } = useGlobalContext();
-  return <div>Navbar</div>;
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          {/* <img src={logo} alt="stripe" loading="lazy" className="nav-logo" /> */}
+          <h2>Stripe</h2>
+        </div>
+      </div>
+    </nav>
+  );
 };
